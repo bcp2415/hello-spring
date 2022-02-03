25689d8c993c9cf8ed0a46e3d02abf05f3dab032
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-02-03 15:07:21 -0600

    finishes basic exercise for chapter 10

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -8,13 +8,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("hello")
 public class HelloController {
 
-    // Handles requests at /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring";
-//    }
-
     @GetMapping("goodbye")
     public String goodbye() {
         return "Goodbye, Spring";
@@ -22,8 +15,8 @@ public class HelloController {
 
     // Create a handler that handles requests of form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String lang) {
+        return createMessage(name, lang);
     }
 
     // Create a handler that handles requests of form /hello/LaunchCode
@@ -38,10 +31,31 @@ public class HelloController {
                 "<body>" +
                 "<form action='/hello' method='POST'>" +  // submit request to /hello
                 "<input type='text' name='name'>" +
+                "<select name='lang' id='lang-select'>" +
+                "<option value='en'>English</option>" +
+                "<option value='ro'>Romanian</option>" +
+                "<option value='la'>Latin</option>" +
+                "<option value='ger'>German</option>" +
+                "<option value='hu'>Hungarian</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    public static String createMessage(String name, String lang) {
+        if (lang.equals("en")) {
+            return "Hello, " + name + "!";
+        } else if (lang.equals("ro")) {
+            return "Bună ziua, " + name + "!";
+        } else if (lang.equals("la")) {
+            return "Salve, " + name + "!";
+        } else if (lang.equals("ger")) {
+            return "Guten Tag, " + name + "!";
+        } else if (lang.equals("hu")) {
+            return "Szia, " + name + "!";
+        } else return "Get lost!";
     }
 
 }

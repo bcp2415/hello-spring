5aeab3322c3ea8f074d285d7bcb1de3ff4f3b226
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-02-13 14:15:22 -0600

    finishes coding along with chapter 11 up to section 5

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -1,26 +1,43 @@
 package org.launchcode.hellospring.controllers;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloSpringController {
 
-    // Create a handler that handles requests of form /hello?name=LaunchCode
+    // Create a handler that handles requests of form/hello?name=LaunchCode
     @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // responds to /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("George");
+        names.add("Laurel");
+        names.add("Mr. Fields");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
